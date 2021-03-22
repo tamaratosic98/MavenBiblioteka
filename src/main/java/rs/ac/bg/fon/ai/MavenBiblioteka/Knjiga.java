fcaddde5e72ec6773ba38de1fae6096dd544f5f6
tamaratosic98 <tosict98@gmail.com>
2021-03-22 15:11:54 +0100

    Maven biblioteka postavke.

---
+++ src/main/java/rs/ac/bg/fon/ai/MavenBiblioteka/Knjiga.java
@@ -0,0 +1,17 @@
+package rs.ac.bg.fon.ai.MavenBiblioteka;
+
+public class Knjiga {
+	private String naslov;
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public void setNaslov(String naslov) {
+		if(naslov==null) {
+			throw new NullPointerException();
+		}
+		this.naslov = naslov;
+	}
+	
+}
